54fdaaaf04d5528b1650d793e025f03dd4811246
Jeremy Battle <battlejj@gmail.com>
2013-09-10 19:09:44 -0400

    0.2.0 non-standard ports now supported
    The plugin now supports sites not run on 80/443 now.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var parseUrl = require('url').parse;
 exports = module.exports = function(req, res, next){
   if(!req.secure){
-    res.redirect('https://' + req.header('Host') + req.url);
+    var httpsPort = req.app.get('httpsPort') || 443;
+    var fullUrl = parseUrl('http://' + req.header('Host') + req.url);
+    res.redirect('https://' + fullUrl.hostname + ':' + httpsPort + req.url);
   } else {
     next();
   }
