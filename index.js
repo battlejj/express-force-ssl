c4b5a66883555f5d33409ea01015048a2e011f5e
Emil Bay <github@tixz.dk>
2014-08-15 11:36:34 +0200

    Use HTTP code 301
    This works well with express 4.x, however I'd recommend using HTTP code 301 "Moved Permanently"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports = module.exports = function(req, res, next){
   if(!req.secure){
     var httpsPort = req.app.get('httpsPort') || 443;
     var fullUrl = parseUrl('http://' + req.header('Host') + req.url);
-    res.redirect('https://' + fullUrl.hostname + ':' + httpsPort + req.url);
+    res.redirect(301, 'https://' + fullUrl.hostname + ':' + httpsPort + req.url);
   } else {
     next();
   }
