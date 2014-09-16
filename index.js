93765a4d1ce063265fdc9b73d5d68d1bead81fbb
Ron <ronco@costite.com>
2014-09-16 16:56:52 -0600

    check 'x-forwarded-proto' for 'https' as well

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var parseUrl = require('url').parse;
+var isSecure = function(req) {
+  if (req.secure) {
+    return true;
+  } else if (req.get('X-Forwarded-Proto') === 'https') {
+    return true;
+  }
+  return false;
+}
 exports = module.exports = function(req, res, next){
-  if(!req.secure){
+  if(!isSecure(req)){
     var httpsPort = req.app.get('httpsPort') || 443;
     var fullUrl = parseUrl('http://' + req.header('Host') + req.url);
     res.redirect(301, 'https://' + fullUrl.hostname + ':' + httpsPort + req.url);
