97cb43d4b43bc0b9696f4647f95a41144ff23e9f
Jeremy Battle <battlejj@gmail.com>
2014-10-07 14:14:46 -0400

    Added test to ensure port redirection for custom SSL port. Added example server.

---
+++ examples/example.js
@@ -0,0 +1,35 @@
+var express = require('express')
+  , forceSSL = require('./../index')
+  , fs = require('fs')
+  , http = require('http')
+  , https = require('https')
+  , request = require('request')
+  ;
+
+var ssl_options = {
+  key: fs.readFileSync('./test/keys/localhost.key'),
+  cert: fs.readFileSync('./test/keys/localhost.crt'),
+  ca: fs.readFileSync('./test/keys/localhost.crt')
+};
+
+var app = express();
+var server = http.createServer(app);
+var secureServer = https.createServer(ssl_options, app);
+
+app.get('/', function(req, res){
+  res.json({msg: 'accessible by http'});
+});
+app.get('/ssl', forceSSL, function(req, res){
+  res.json({msg: 'only https'});
+});
+
+app.get('/ssl/deep/route/:id', forceSSL, function(req, res){
+  var host = req.headers.host.split(':');
+  var port = host.length > 1 ? host[1] : 'default port';
+  res.json({msg: 'only https, port: ' + port, id: req.param('id')});
+});
+
+app.set('httpsPort', 8443);
+
+secureServer.listen(8443);
+server.listen(8080);
--- test/index.js
+++ test/index.js
@@ -25,10 +25,16 @@ app.get('/ssl', forceSSL, function(req, res){
   res.json({msg: 'only https'});
 });
 
+app.get('/ssl/deep/route/:id', forceSSL, function(req, res){
+  var host = req.headers.host.split(':');
+  var port = host.length > 1 ? host[1] : 'default port';
+  res.json({msg: 'only https, port: ' + port, id: req.param('id')});
+});
+
 app.set('httpsPort', 8443);
 
-secureServer.listen(8443)
-server.listen(8080)
+secureServer.listen(8443);
+server.listen(8080);
 
 describe('Test SSL Redirect', function(){
   it('Should not automatically redirect to SSL for a non Force SSL middleware page.', function(done){
@@ -51,6 +57,37 @@ describe('Test SSL Redirect', function(){
     }, function (error, response, body) {
       expect(error).to.not.exist;
       expect(response.statusCode).to.equal(301);
+      done();
+    });
+  });
+
+  it('Should redirect to SSL on correct port for a Force SSL middleware page.', function(done){
+    request.get({
+      url: 'http://localhost:8080/ssl',
+      followRedirect: true,
+      strictSSL: false
+    }, function (error, response, body) {
+      expect(error).to.not.exist;
+      expect(response.statusCode).to.equal(200);
+      expect(response.request.uri.port).to.equal(app.get('httpsPort').toString());
+      done();
+    });
+  });
+
+  it('Should redirect to SSL on correct port for a nested URL with Force SSL middleware.', function(done){
+    request.get({
+      url: 'http://localhost:8080/ssl/deep/route/50',
+      followRedirect: true,
+      strictSSL: false
+    }, function (error, response, body) {
+      expect(error).to.not.exist;
+      expect(response.statusCode).to.equal(200);
+      expect(response.request.uri.port).to.equal(app.get('httpsPort').toString());
+      body = JSON.parse(body);
+      expect(body).to.have.property('msg');
+      expect(body.msg).to.equal('only https, port: ' + app.get('httpsPort').toString());
+      expect(body).to.have.property('id');
+      expect(body.id).to.equal('50');
       done();
     });
   });
