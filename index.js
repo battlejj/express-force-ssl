375dd85fd742e77aba6d672553ac78a0d1fdda08
Jeremy Battle <battlejj@gmail.com>
2015-07-26 19:49:02 -0400

    Cleaning up the mess

--- index.js
+++ index.js
@@ -1,54 +1,77 @@
 var parseUrl = require('url').parse;
+var assign = require('lodash.assign');
 
-var isSecure = function(req) {
-  var options = req.app.get('forceSSLOptions') || {};
-  var XFPHeader = (req.get('X-Forwarded-Proto') || '').toLowerCase();
-  if (req.secure) {
-    return true;
-  } else if (options.trustXFPHeader && XFPHeader === 'https') {
+function isSecure (secure, xfpHeader, trustXFPHeader) {
+  xfpHeader = xfpHeader ? xfpHeader.toString().toLowerCase() : '';
+  if (secure) {
     return true;
   }
-  return false;
-};
+
+  return trustXFPHeader && xfpHeader === 'https'
+}
+
+function shouldRedirect (redirectsEnabled, method) {
+  if (!redirectsEnabled) {
+    return false
+  }
+
+  return method === "GET";
+}
+
+function checkForDeprecation (appSettings, optionsHttpsPort) {
+  var httpsPort = appSettings.get('httpsPort');
+
+  if (appSettings.get('env') === 'development') {
+    if (httpsPort) {
+      console.warn('express-force-ssl deprecated: app.set("httpsPort", ' + httpsPort + '), use ' +
+        'app.set("forceSSLOptions", { httpsPort: ' + httpsPort + ' }) instead.');
+    }
+
+    if (httpsPort && optionsHttpsPort) {
+      console.warn('You have set both app.get("httpsPort") and app.get("forceSSLOptions").httpsPort ' +
+        'Your app will use the value in forceSSLOptions.');
+    }
+  }
+}
 
 module.exports = function(req, res, next){
+  var redirect;
+  var secure;
+  var xfpHeader = req.get('X-Forwarded-Proto');
+  var localHttpsPort = res.locals['httpsPort'];
+  var appHttpsPort = req.app.get('httpsPort');
+  var httpsPort;
+  var fullUrl;
+  var redirectUrl;
 
   var options = {
     trustXFPHeader: false,
     enable301Redirects: true,
-    httpsPort: false
+    httpsPort: false,
+    sslRequiredMessage: 'SSL Required.'
   };
 
   var expressOptions = req.app.get('forceSSLOptions') || {};
+  var localOptions = res.locals;
 
-  for(var i in options){
-    if(options.hasOwnProperty(i) && Object.keys(res.locals).indexOf(i) > -1){
-      options[i] = res.locals[i];
-    } else if(options.hasOwnProperty(i) && expressOptions.hasOwnProperty(i)){
-      options[i] = expressOptions[i];
-    }
-  }
+  assign(options, expressOptions, localOptions);
 
-  if(!isSecure(req)){
-    if(req.method === "GET" && options.enable301Redirects){
-      if(req.app.get('env') === 'development') {
-        if (req.app.get('httpsPort')) {
-          console.warn('express-force-ssl deprecated: app.set("httpsPort", ' + req.app.get('httpsPort') + '), use ' +
-            'app.set("forceSSLOptions", { httpsPort: ' + req.app.get('httpsPort') + ' }) instead.');
-        }
+  secure = isSecure(req.secure, xfpHeader, options.trustXFPHeader);
+  redirect = shouldRedirect(options.enable301Redirects, req.method);
 
-        if(req.app.get('httpsPort') && options.httpsPort){
-          console.warn('You have set both app.get("httpsPort") and app.get("forceSSLOptions").httpsPort ' +
-            'Your app will use the value in forceSSLOptions.');
-        }
-      }
-      var httpsPort = res.locals['httpsPort'] || options.httpsPort || req.app.get('httpsPort') || 443;
-      var fullUrl = parseUrl(req.protocol + '://' + req.header('Host') + req.originalUrl);
+  if (!secure) {
+    if (redirect) {
+      checkForDeprecation(req.app, options.httpsPort);
+
+      httpsPort = localHttpsPort || options.httpsPort || appHttpsPort || 443;
+      fullUrl = parseUrl(req.protocol + '://' + req.header('Host') + req.originalUrl);
+
       //intentionally allow coercion of https port
-      var redirectUrl = 'https://' + fullUrl.hostname + (httpsPort == 443 ? '' : (':' + httpsPort)) + req.originalUrl;
+      redirectUrl = 'https://' + fullUrl.hostname + (httpsPort == 443 ? '' : (':' + httpsPort)) + req.originalUrl;
+
       res.redirect(301, redirectUrl);
     } else {
-      res.status(403).send('SSL Required.');
+      res.status(403).send(options.sslRequiredMessage);
     }
   } else {
     next();
